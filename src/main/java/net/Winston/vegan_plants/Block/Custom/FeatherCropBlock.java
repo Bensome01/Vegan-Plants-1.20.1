ed61189f3b7875df2ec28b8520592c593b2a340d
Bensome01 <minicrafter.log@gmail.com>
2025-01-12 22:48:15 -0800

    change feather crop age to 15 and fixed voxel shape

--- src/main/java/net/Winston/vegan_plants/Block/Custom/FeatherCropBlock.java
+++ src/main/java/net/Winston/vegan_plants/Block/Custom/FeatherCropBlock.java
@@ -16,10 +16,36 @@ import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.block.state.StateDefinition;
 import net.minecraft.world.level.block.state.properties.BlockStateProperties;
 import net.minecraft.world.level.block.state.properties.IntegerProperty;
+import net.minecraft.world.phys.shapes.CollisionContext;
+import net.minecraft.world.phys.shapes.VoxelShape;
 
 public class FeatherCropBlock extends CropBlock{
-    public static final int MAX_AGE = 7;
-    public static final IntegerProperty AGE = BlockStateProperties.AGE_7;
+    public static final int MAX_AGE = 15;
+    public static final IntegerProperty AGE = BlockStateProperties.AGE_15;
+    private static final VoxelShape[] SHAPE_BY_AGE = new VoxelShape[]
+    {
+        Block.box(0.0D, 0.0D, 0.0D, 16.0D, 3.0D, 16.0D),
+        Block.box(0.0D, 0.0D, 0.0D, 16.0D, 3.0D, 16.0D),
+        Block.box(0.0D, 0.0D, 0.0D, 16.0D, 7.0D, 16.0D),
+        Block.box(0.0D, 0.0D, 0.0D, 16.0D, 7.0D, 16.0D),
+        Block.box(0.0D, 0.0D, 0.0D, 16.0D, 7.0D, 16.0D),
+        Block.box(0.0D, 0.0D, 0.0D, 16.0D, 7.0D, 16.0D),
+        Block.box(0.0D, 0.0D, 0.0D, 16.0D, 10.0D, 16.0D),
+        Block.box(0.0D, 0.0D, 0.0D, 16.0D, 10.0D, 16.0D),
+        Block.box(0.0D, 0.0D, 0.0D, 16.0D, 10.0D, 16.0D),
+        Block.box(0.0D, 0.0D, 0.0D, 16.0D, 10.0D, 16.0D),
+        Block.box(0.0D, 0.0D, 0.0D, 16.0D, 14.0D, 16.0D),
+        Block.box(0.0D, 0.0D, 0.0D, 16.0D, 14.0D, 16.0D),
+        Block.box(0.0D, 0.0D, 0.0D, 16.0D, 14.0D, 16.0D),
+        Block.box(0.0D, 0.0D, 0.0D, 16.0D, 14.0D, 16.0D),
+        Block.box(0.0D, 0.0D, 0.0D, 16.0D, 14.0D, 16.0D),
+        Block.box(0.0D, 0.0D, 0.0D, 16.0D, 15.0D, 16.0D)
+    };
+
+    @Override
+    public VoxelShape getShape(BlockState p_52297_, BlockGetter p_52298_, BlockPos p_52299_, CollisionContext p_52300_) {
+      return SHAPE_BY_AGE[this.getAge(p_52297_)];
+   }
 
     public FeatherCropBlock(Properties pProperties) {
         super(pProperties);
