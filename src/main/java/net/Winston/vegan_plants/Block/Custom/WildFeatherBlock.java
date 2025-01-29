39b9de71d3895d45d61fc5835b38e5bff6f89813
Bensome01 <minicrafter.log@gmail.com>
2025-01-28 21:52:37 -0800

    override performBonemeal method

--- src/main/java/net/Winston/vegan_plants/Block/Custom/WildFeatherBlock.java
+++ src/main/java/net/Winston/vegan_plants/Block/Custom/WildFeatherBlock.java
@@ -1,6 +1,10 @@
 package net.Winston.vegan_plants.Block.Custom;
 
+import net.minecraft.core.BlockPos;
+import net.minecraft.server.level.ServerLevel;
+import net.minecraft.util.RandomSource;
 import net.minecraft.world.level.block.TallGrassBlock;
+import net.minecraft.world.level.block.state.BlockState;
 
 public class WildFeatherBlock extends TallGrassBlock {
 
@@ -8,4 +12,7 @@ public class WildFeatherBlock extends TallGrassBlock {
         super(pProperties);
     }
 
+    @Override
+    public void performBonemeal(ServerLevel p_222578_, RandomSource p_222579_, BlockPos p_222580_, BlockState p_222581_)
+    {}
 }
